31fd9fd6d2e16ca6684ccce91fd504bfbfb01aa8
monacodev <nassim.chabane@gmail.com>
2021-04-11 18:23:35 +0200

    boucle pour créer un triangle

--- src/TriangleShape2.java
+++ src/TriangleShape2.java
@@ -21,6 +21,23 @@ public class TriangleShape2 {
 				size = 2;
 			}
 			
+			// Boucle for pour créer le triangle
+
+						for (int ligne = 0; ligne < size; ligne++) { // Boucle externe loop outern
+							for (int i = 0; i < ligne; i++) {
+								System.out.print(" ");
+							}
+							for (int j = 0; j < size - ligne; j++) { // Boucle interne loop inside
+								System.out.print("*");
+							}
+
+							for (int j = 1; j < size - ligne; j++) { // Boucle interne loop inside
+								System.out.print("*");
+							}
+							System.out.println(); // Saut de ligne après chaque tour de la boucle externe
+						}
+
+			
 		} catch (InputMismatchException e) { // exception si on saisie un mauvais format
 			System.out.println("La saisie ne correspond pas au bon format, le nombre doit être un entier ");
 		}
