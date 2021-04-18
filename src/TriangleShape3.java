e6c0d2b4e8a48b655dfb61c7880155835af8e966
monacodev <nassim.chabane@gmail.com>
2021-04-18 17:15:37 +0200

    Boucle interne pour remplir les lignes d'étoiles

--- src/TriangleShape3.java
+++ src/TriangleShape3.java
@@ -1,13 +1,23 @@
 
 public class TriangleShape3 {
-	
-	public void displayTraingleRec() {
+	/*
+	 * procédure qui nous affiche un triangle réctangle avec le sommet en haut
+	 */
+	public static void displayTraingleRec() {
+
+		for (int i = 0; i < 5; i++) { // boucle externe
+			
+			for (int j = 0; j <= i; j++) { // boucle interne
+				System.out.print("*");
+			}
+			System.out.println(); // saut de ligne à chaque sortie de la boucle interne
+		}
 		
+
 	}
-	
+
 	public static void main(String[] args) {
-		
-		
+		displayTraingleRec();
 	}
 
 }
