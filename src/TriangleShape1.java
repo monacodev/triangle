d30bb8a6464c84f1641be75c8861dabeb9cf738a
monacodev <43535390+monacodev@users.noreply.github.com>
2023-07-21 11:45:19 +0200

    Update TriangleShape1.java

--- src/TriangleShape1.java
+++ src/TriangleShape1.java
@@ -5,7 +5,7 @@ public class TriangleShape1 {
 
 		for (int line = 1; line <= 5; line++) {
 			for (int i = 5 - line; i > 0; i--) {
-				System.out.print("."); // les étoiles sont affichés 10 fois les unes à coté de l'autre car print ne  fait pas de retour à la ligne 
+				System.out.print("."); // les étoiles sont affichés les unes à coté de l'autre car print ne fait pas de retour à la ligne 
 			}
 			System.out.println(line);// quand on sort de la boucle interne on fait un saut de ligne
 		}
